4f01e15acbb091698d4150b1cfe10f268d785c03
yuki <yuki.onishi1129@gmail.com>
2021-11-17 01:54:53 +0900

    update: main.go

--- app/main.go
+++ app/main.go
@@ -5,20 +5,10 @@ import (
 	"myapp/router"
 )
 
-// func handler(writer http.ResponseWriter, _ *http.Request) {
-// 	fmt.Fprint(writer, "Hello World")
-// }
-
 func main() {
-	// http.HandleFunc("/", handler)
-	// http.ListenAndServe(":3000", nil)
-	// godotenv.Load(".env")
-	// db.Migrate()
-
 	// DB接続
 	db.Init()
 
 	// API起動
-	// controllers.StartWebServer()
 	router.StartWebServer()
 }
