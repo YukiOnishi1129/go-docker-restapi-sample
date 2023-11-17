3b87b1e0e826abdfe35318b70297bca479ae6ea5
yukionishi1129 <yuki.onishi1129@gmail.com>
2023-11-17 23:19:37 +0900

    update: lint error

--- Makefile
+++ Makefile
@@ -29,3 +29,8 @@ db-seed:
 ## 複数のライブラリを指定する場合は、name="xxx yyy" のように""で囲んで実行すること
 go-add-library:
 	docker exec -it ${BACKEND_CONTAINER_NAME} sh -c "go get ${name}"
+## テスト
+test:
+	docker exec -it ${BACKEND_CONTAINER_NAME} sh -c "go test -v ./..."
+lint:
+	docker exec -it ${BACKEND_CONTAINER_NAME} sh -c "staticcheck ./..."
--- app/services/auth.service_test.go
+++ app/services/auth.service_test.go
@@ -1,35 +1,30 @@
 package services_test
 
-import (
-	"myapp/models"
-	"myapp/repositories"
-	"myapp/utils/logic"
-	"myapp/utils/validation"
-)
+// lintエラーのため一旦コメントアウト
 
-type userRepoMock struct {
-	repositories.UserRepository
-	FakeGetUserByEmail func(user *models.User, email string) error
-	FakeGetAllUserByEmail func(users *[]models.User, email string) error
-	FakeCreateUser func(createUsers *models.User) error
-}
+//type userRepoMock struct {
+//	repositories.UserRepository
+//	FakeGetUserByEmail    func(user *models.User, email string) error
+//	FakeGetAllUserByEmail func(users *[]models.User, email string) error
+//	FakeCreateUser        func(createUsers *models.User) error
+//}
 
-type authLogicMock struct {
-	logic.AuthLogic
-}
-
-type userLogicMock struct {
-	logic.UserLogic
-}
-
-type responseLogicMock struct {
-	logic.ResponseLogic
-}
-
-type jwtLogicMock struct {
-	logic.JWTLogic
-}
-
-type authValidation struct {
-	validation.AuthValidation
-}
+//type authLogicMock struct {
+//	logic.AuthLogic
+//}
+//
+//type userLogicMock struct {
+//	logic.UserLogic
+//}
+//
+//type responseLogicMock struct {
+//	logic.ResponseLogic
+//}
+//
+//type jwtLogicMock struct {
+//	logic.JWTLogic
+//}
+//
+//type authValidation struct {
+//	validation.AuthValidation
+//}
